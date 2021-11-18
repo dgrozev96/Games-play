3dbaedce4159b3a75ba4e915513446aef2262f75
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-11-18 21:08:07 +0200

    show no games yet message

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -2,23 +2,34 @@ import { useEffect, useState } from "react";
 import GameCard from './GameCard'
 const GameCatalog = () => {
     const [games, setGames] = useState([])
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
-            .then(result => {
-                setGames(result);
-            })
+        setLoading(true);
+
+        setTimeout(() => {
+            fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+                .then(res => res.json())
+                .then(result => {
+                    setGames(result);
+                    setLoading(false)
+                })
+        }, 1000);
     }, [])
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.map(x => <GameCard game={x} />)}
+            {loading
+                ? <p>Loading...</p>
+                : games.length > 0
+                    ? games.map(x => <GameCard key={x._id} game={x} />)
+                    : < h3 className="no-articles">No games yet</h3>
+            }
 
 
-            <h3 className="no-articles">No games yet</h3>
-        </section>
+
+        </section >
     )
 };
 
