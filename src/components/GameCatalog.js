93ea2b14470c0e6351a6f4ef4482749b43e703f4
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-11-18 20:18:41 +0200

    added game cards

--- src/components/GameCatalog.js
+++ src/components/GameCatalog.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
 const GameCatalog = () => {
+    const [games, setGames] = useState([])
+    useEffect = () => {
+        useEffect(() => {
+            fetch('http://localhost:3000/data/games?sortBy=_createdOn%20desc')
+                .then(res => res.json())
+                .then(result => {
+                    setGames(result);
+                })
+        }, [])
+    }
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
@@ -11,23 +22,7 @@ const GameCatalog = () => {
                 </div>
 
             </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+            
 
             <h3 className="no-articles">No articles yet</h3>
         </section>
